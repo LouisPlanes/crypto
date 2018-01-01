fc46c2b6659212bbe73c8ad93c8cbc862fe4c3c1
kaelhanas <gkylian@gmail.com>
2018-01-01 18:20:05 +0100

    Debut de la reecriture de l'agenda pour que la Hashmap soir du type HashMap<Integer, List<Triple>>

---
+++ Agenda/src/agenda/Agenda2.java
@@ -0,0 +1,100 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package agenda;
+
+import java.awt.Canvas;
+import java.awt.Graphics;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+/**
+ *
+ * @author Khoob (Gabin MICHALET)
+ */
+public class Agenda2 extends Canvas{
+    private HashMap<Integer, List<Triple>> calendar = new HashMap();
+    private ArrayList ls = new ArrayList();
+    private Triple aFaireJour = new Triple();
+    
+    public Agenda2() {super.setSize(100,20);}
+  
+    public int transform(String date) 
+    {
+        String[] tmp = date.split("/");
+        int temp = Integer.parseInt(tmp[0]) + Integer.parseInt(tmp[1]) * 100 + Integer.parseInt(tmp[2]) * 10000; 
+        return temp;
+    }
+    
+    public void addDayListener(DayListener l) { ls.add(l); }
+    public void removeDayListener(DayListener l) { ls.remove(l); }
+    public void fireDayEvent(DayEvent e) {
+        Iterator i = ls.iterator();
+        while (i.hasNext())
+            ((DayListener)i.next()).day(e);
+    }
+    
+    
+/*      map.put(20180101,new ArrayList<Triple>());
+    	map.get(20180101).add(new Triple("08h00", "04h00", "Reunion"));
+    	map.get(20180101).add(new Triple("12h00", "02h00", "Repas"));
+    	map.put(20180102,new ArrayList<Triple>());
+    	map.get(20180102).add(new Triple("14h00", "04h00", "Reunion2"));*/
+    
+    public void setEvent(String date, Triple info) {
+        //int temp = transform(date);
+        //this.calendar.put(transform(date), info);
+        int key = transform(date);
+        if(!this.calendar.containsKey(key))
+        {
+            this.calendar.put(key, new ArrayList<Triple>());
+        }
+        this.calendar.get(key).add(info);
+    }
+    
+    public List<Triple> getEventList (String date) {
+
+        List<Triple> result = new ArrayList();
+        for(Map.Entry<Integer, List<Triple>> entry : this.calendar.entrySet())
+        {
+            if(entry.getKey() == (transform(date)))
+            {
+                result = (entry.getValue());
+            }
+        }
+        
+        return result;
+    }
+    public void setAFaireJour(String heure,String duree,String descevent) {
+        aFaireJour.setHeure(heure);
+        aFaireJour.setDuree(duree);
+        aFaireJour.setDescevent(descevent);
+    } 
+    public Triple getAFaireJour() {
+        return aFaireJour;
+    }
+    public void setDay(String date, Triple txt) {
+        setEvent(date,txt);
+    }
+    
+    public void getDay(String date) { 
+        aFaireJour=getEventList(date);              //Need gabin pour comprendre
+        this.fireDayEvent(new DayEvent(this));
+    }
+    
+    public void paint(Graphics g){
+        g.setFont(java.awt.Font.getFont(java.awt.Font.DIALOG));
+        g.drawString("Agenda", 10, 10);
+    }
+    
+    public HashMap<Integer, List<Triple>> getCalendar()
+    {
+        return calendar;
+    }
+    
+}
